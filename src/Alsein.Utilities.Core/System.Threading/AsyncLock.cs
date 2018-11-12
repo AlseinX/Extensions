1299b389b93806c4cbe3c0a741b91171ee22f45b
Alsein <xyh951115@live.com>
2018-11-13 10:44:11 +1100

    AsyncLock with finalizer

--- src/Alsein.Utilities.Core/System.Threading/AsyncLock.cs
+++ src/Alsein.Utilities.Core/System.Threading/AsyncLock.cs
@@ -3,16 +3,16 @@ using System.Threading.Tasks;
 namespace System.Threading
 {
     /// <summary>
-    /// 
+    /// A async lock for using statement.
     /// </summary>
     public class AsyncLock
     {
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
         /// <summary>
-        /// 
+        /// Must 'using' the await result of this method, or It may cause problems.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The System.IDisposible object that releases the lock when disposed.</returns>
         public async Task<IDisposable> WaitAsync()
         {
             await _semaphore.WaitAsync();
@@ -27,10 +27,16 @@ namespace System.Threading
 
             public void Dispose()
             {
+                if (_target == null)
+                {
+                    return;
+                }
                 var temp = _target;
                 _target = null;
-                temp?._semaphore?.Release();
+                temp._semaphore.Release();
             }
+
+            ~Disposer() => Dispose();
         }
     }
 }
