48abb753ff3fc83a163e5436710f289ac58ec368
AlseinX <xyh951115@live.com>
2018-12-06 03:27:14 +1100

    nuget-push.sh add argument passing

--- nuget-push.sh
+++ nuget-push.sh
@@ -3,6 +3,6 @@ git clean -xfd
 dotnet pack -c Release
 for filename in dist/*.nupkg
 do
-    dotnet nuget push "$filename" -s https://api.nuget.org/v3/index.json
+    dotnet nuget push "$filename" -s https://api.nuget.org/v3/index.json $@
 done
 rm -rf dist
