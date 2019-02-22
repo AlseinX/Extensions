573296fcc444cdacbf56008b96dd9872e2d30a21
AlseinX <xyh951115@live.com>
2019-02-22 11:51:30 +0800

    Contexted

---
+++ src/Alsein.Utilities.Contexted/ContextNotPresetException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alsein.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ContextNotPresetException : Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ContextNotPresetException() : base("Context must be set before instantiation.") { }
+    }
+}
